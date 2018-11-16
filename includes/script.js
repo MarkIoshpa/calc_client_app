afb2f9f1c3362dda0073af6a78616072daa0b568
Mark <mark.ioshpa@gmail.com>
2018-11-16 07:52:19 +0200

    calculator client

--- includes/script.js
+++ includes/script.js
@@ -1,27 +1,29 @@
-(function () {
+$( "#postBtn" ).click(function() {
     $.post("http://shenkar.html5-book.co.il/2018-2019/dcs/dev_26/service_calculator/Calculator_Service.php",
-    {func:"avg",num1:10,num2:15,num3:20},
+    {func:$("#func").val(),num1:$("#num1").val(),num2:$("#num2").val(),num3:$("#num3").val()},
     function( data ) {
-        $(".resultPost").append(data.retVal);
+        $(".result").text("Result (Post): "+data.retVal);
          console.log( "Return data: " + data.retVal);
     });
-})();
+});
 
-(function () {
-    var dataObj = {'func':'sum','num1':1,'num2':2,'num3':0};
+$( "#putBtn" ).click(function() {
+    var dataObj = {'func':$("#func").val(),'num1':$("#num1").val(),'num2':$("#num2").val(),'num3':$("#num3").val()};
     $.ajax({
         url: 'http://shenkar.html5-book.co.il/2018-2019/dcs/dev_26/service_calculator/Calculator_Service.php',
         data:dataObj,
         type: 'PUT',
         success: function(data) {
-            $(".resultPut").append(data.retVal);
+            $(".result").text("Result (Put): "+data.retVal);
             console.log("Return data: " + data.retVal);
         }
     });
-})();
+});
 
-$.get("http://shenkar.html5-book.co.il/2018-2019/dcs/dev_26/service_calculator/Calculator_Service.php?func=mult&num1=10&num2=5&num3=2",
-function(data) {
-        $(".resultGet").append(data.retVal);
-        console.log( "Return data: " + data.retVal);
+$( "#getBtn" ).click(function() {
+    $.get("http://shenkar.html5-book.co.il/2018-2019/dcs/dev_26/service_calculator/Calculator_Service.php?func="+$("#func").val()+"&num1="+$("#num1").val()+"&num2="+$("#num2").val()+"&num3="+$("#num3").val()+"",
+    function(data) {
+            $(".result").text("Result (Get): "+data.retVal);
+            console.log( "Return data: " + data.retVal);
+    });
 });
